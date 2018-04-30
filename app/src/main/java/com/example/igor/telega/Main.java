d4cd4594e3891d00ae15d2b5ea2cb6fe16ba9f2a
Igor Anokhin <igor.anohin@emlid.com>
2018-04-30 17:15:44 +0300

    Main: add best score

--- app/src/main/java/com/example/igor/telega/Main.java
+++ app/src/main/java/com/example/igor/telega/Main.java
@@ -1,6 +1,8 @@
 package com.example.igor.telega;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,12 +14,22 @@ import android.widget.TextView;
 
 public class Main extends AppCompatActivity {
     private TextView count;
+    private TextView bestcount;
+    private int best_score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        count = findViewById(R.id.bestScore);
+        count = findViewById(R.id.currentScore);
+        bestcount = findViewById(R.id.bestScore);
+
+        SharedPreferences prefs = this.getSharedPreferences("myPrefsKey", Context.MODE_PRIVATE);
+        best_score = prefs.getInt("key", 0);
+        String sc = Integer.toString(best_score);
+        bestcount.setText(sc);
+        count.setText("0");
+
         TextView rule = findViewById(R.id.rule);
 
         Button start_button = findViewById(R.id.start_button);
@@ -49,5 +61,13 @@ public class Main extends AppCompatActivity {
         if (data == null) return;
         String result = data.getStringExtra("count");
         count.setText(result);
+
+        if (best_score < Integer.parseInt(result)) {
+            SharedPreferences prefs = this.getSharedPreferences("myPrefsKey", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putInt("key", Integer.parseInt(result)).apply();
+            editor.commit();
+            bestcount.setText(result);
+        }
     }
 }
