a7fdbcb229f98631557264bc886d326c7da0b963
PixarV <pixarvv@yandex.ru>
2018-04-30 14:30:31 +0000

    start button #2

--- app/src/main/java/com/example/igor/telega/Main.java
+++ app/src/main/java/com/example/igor/telega/Main.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -15,8 +18,13 @@ public class Main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         count = findViewById(R.id.bestScore);
+        TextView rule = findViewById(R.id.rule);
 
-        Button start_button = (Button) findViewById(R.id.start_button);
+        Button start_button = findViewById(R.id.start_button);
+
+        start_button.startAnimation(getAnimation(false));
+        rule.startAnimation(getAnimation(true));
+
         start_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -25,6 +33,16 @@ public class Main extends AppCompatActivity {
             }
         });
     }
+
+    private Animation getAnimation(boolean direct) {
+        Animation animation = direct ? new AlphaAnimation(0.0f, 1.0f) : new AlphaAnimation(1.0f, 0.0f);
+        animation.setDuration(4000);
+        animation.setRepeatMode(Animation.REVERSE);
+        animation.setRepeatCount(Animation.INFINITE);
+        return animation;
+    }
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
